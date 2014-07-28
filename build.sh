55875904248831f8a9135baeeaab358cdc4b2655
Jason Slack-Moehrle <slackmoehrle@gmail.com>
2014-07-28 11:17:46 -0700

    initial commit

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "this script reqires: "
+echo ""
+echo "Pandoc: http://johnmacfarlane.net/pandoc/getting-started.html"
+echo "A LaTex Distribution: http://www.tug.org/mactex/downloading.html"
+echo "run: sudo tlmgr install ec ecc -- to install missing fonts"
+echo ""
+echo "To Do: Be able to insert a page break at the end of each chapter. right now it is continuous"
+
+pandoc outline.md -s -o outline.pdf
+
+pandoc 1.md 2.md 3.md 4.md 5.md 6.md 7.md 8.md 9.md 10.md 11.md 12.md 13.md A.md B.md C.md D.md E.md F.md G.md H.md -s -o manual.pdf
