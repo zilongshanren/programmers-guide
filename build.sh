4a58a0cebd12c827ea4a9cbe8c658088562b692d
Slack-Moehrle <slackmoehrle@gmail.com>
2014-11-12 19:34:16 -0800

    copy new resources

--- build.sh
+++ build.sh
@@ -41,7 +41,8 @@ pandoc -s --template "_layout" --css "solarized-light.css" -f markdown -t html5 
 pandoc -s --template "_layout" --css "solarized-light.css" -f markdown -t html5 -o _output/html/I.html I.md
 
 echo "copying the html resources..."
-cp main.html index.html solarized-light.css preview.png _output/html/.
+cp main.html index.html solarized-light.css main.css preview.png m_toc.html ch*.html _output/html/.
+
 cp -r ./2 _output/html/.
 cp -r ./3 _output/html/.
 cp -r ./4 _output/html/.
@@ -116,59 +117,3 @@ _output/html/blank.html
 ### Building the PDF version
 echo "building the PDF version..."
 pandoc -s -o _output/html/ProgrammersGuide.pdf _output/html/ProgrammersGuide.epub
-
-
-### DOnt remove below here right yet.
-
-
-#echo "building the table of contents..."
-#rm -rf toc.txt
-#
-#./toc.pl _output/html/1.html > _output/html/1.txt
-#./toc.pl _output/html/2.html > _output/html/2.txt
-#./toc.pl _output/html/3.html > _output/html/3.txt
-#./toc.pl _output/html/4.html > _output/html/4.txt
-#./toc.pl _output/html/5.html > _output/html/5.txt
-#./toc.pl _output/html/6.html > _output/html/6.txt
-#./toc.pl _output/html/7.html > _output/html/7.txt
-#./toc.pl _output/html/8.html > _output/html/8.txt
-#./toc.pl _output/html/9.html > _output/html/9.txt
-#./toc.pl _output/html/10.html > _output/html/10.txt
-#./toc.pl _output/html/11.html > _output/html/11.txt
-#./toc.pl _output/html/12.html > _output/html/12.txt
-#./toc.pl _output/html/13.html > _output/html/13.txt
-#./toc.pl _output/html/14.html > _output/html/14.txt
-
-#cd _output/html/
-#awk 'FNR==1{print ""}1' 1.txt 2.txt 3.txt 4.txt 5.txt 6.txt 7.txt 8.txt 9.txt 10.txt 11.txt 12.txt 13.txt 14.txt > toc.md
-#rm -rf *.txt
-#cd ../..
-#pandoc -s --template "_layout" --css "style.css" -f markdown -t html5 -o _output/html/toc.html _output/html/toc.md
-
-
-
-#pandoc --toc --toc-depth=3 -markdown_github -s -o _output/html/toc.html \
-#_output/html/1.html \
-#2.md \
-#3.md \
-#4.md
-
-#pandoc "README.md" --toc --standalone --smart --toc --number-sections  --template default.html5 --output="_output/html/README.html"
-
-#pandoc -markdown_github -s -o outline.pdf outline.md
-
-#pandoc --chapters --latex-engine=xelatex -markdown_github -s -o manual.pdf title.md 1.md 2.md 3.md 4.md
-
-#pandoc --toc --highlight-style=kate -V documentclass=book -markdown_github -s -o manual.html title.md 1.md 2.md 3.md 4.md
-
-#pandoc --toc --highlight-style=kate -V documentclass=report -markdown_github -s
-#-o manual.html title.md 1.md 2.md 3.md 4.md
-
-#pandoc -markdown_github -s -o manual.pdf title.md 1.md 2.md 3.md 4.md
-
-
-#pandoc -S --epub-stylesheet=style.css -o manual.epub title.md 1.md 2.md 3.md 4.md
-
-#pandoc -s --template "_layout" --css "style.css" -f markdown -t html5 -o "$@" "$<" 1.md 2.md
-
-#pandoc -s -o manual.pdf manual.epub
