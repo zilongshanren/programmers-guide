ced6d0a3ca838c718377d843aaf3e380506c1314
Slack-Moehrle <slackmoehrle@gmail.com>
2014-12-18 11:24:49 -0800

    edits

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ echo "To Do: Be able to insert a page break at the end of each chapter. right no
 echo ""
 
 ### Define variables that we need for this script
-chapters=('blank' 'index' '1' '2' '3' '4' '5' '6' '7' '8' '9' '10' '11' '12' '13' '14' 'A' 'B'
+chapters=('blank' 'index' '1' '2' '3' '4' '5' '8' '9' '13' 'A' 'B'
 'C' 'D' 'E' 'F' 'G' 'H' 'I')
 
 ### Turn on globbing (BASH 4 required)
@@ -28,17 +28,17 @@ echo "copying resources to docs..."
 mkdir -p docs
 mkdir -p print
 rsync -a chapters/*-web* docs/
-#rsync -a chapters/*-print* print/
+rsync -a chapters/*-print* print/
 
 ### Pre-process the markdown files
 ### for the web = -DWEB
 ### without -DWEB = print
 ### Do every chapter, just in case images get added we dont have to modify script
-echo "pre-processing chapters for the web..."
-for i in ${chapters[@]}; do
-  gpp -H -DWEB chapters/${i}.md -o docs/${i}.md
-  gpp -H chapters/${i}.md -o print/${i}.md
-done
+#echo "pre-processing chapters for the web..."
+#for i in ${chapters[@]}; do
+#  gpp -H -DWEB chapters/${i}.md -o docs/${i}.md
+#  gpp -H chapters/${i}.md -o print/${i}.md
+#done
 
 ### Now we can use MKDocs to build the static content
 echo "MKDocs Build..."
@@ -68,24 +68,24 @@ blank.html \
 4.html \
 blank.html \
 5.html \
-blank.html \
-6.html \
-blank.html \
-7.html \
+#blank.html \
+#6.html \
+#blank.html \
+#7.html \
 blank.html \
 8.html \
 blank.html \
 9.html \
 #blank.html \
 #10.html \
-blank.html \
-11.html \
-blank.html \
-12.html \
+#blank.html \
+#11.html \
+#blank.html \
+#12.html \
 blank.html \
 13.html \
-blank.html \
-14.html \
+#blank.html \
+#14.html \
 blank.html \
 A.html \
 blank.html \
